1349fd3f1bfd0b70c5705d4f36e0d36d5a096559
oHayashi <whale.ice26flat0@gmail.com>
2018-07-27 01:49:27 +0900

    FirstCommit

---
+++ FirstApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FirstApp.Models;
+
+namespace FirstApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
